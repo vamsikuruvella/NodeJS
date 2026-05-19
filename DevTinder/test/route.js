d1e454e2dda2d4ed288ee1a603c4c776f536a20f
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2026-05-19 09:58:22 +0000

    19/05/26 v2

--- DevTinder/test/route.js
+++ DevTinder/test/route.js
@@ -5,9 +5,13 @@ const app = express();
 app.use('/user',(req,res,next)=>{ 
     // app.use, app.get, app.post etc can take multiple callbacks, res.send will stop the callback there
     // next moves execution to next callback
-    console.log("First app.use")';
+    console.log("First app.use");
     next();
 },(req,res)=>{
     console.log("second app.use");
     res.send("2nd Response");
+})
+
+app.listen(3000,()=>{
+    console.log("listening one port 3000");
 })
