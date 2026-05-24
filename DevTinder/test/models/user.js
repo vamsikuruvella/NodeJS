b1bc013b640c0e07ae4ce2bf0292343184257125
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2026-05-24 17:34:15 +0530

    24/05/26 v0

--- DevTinder/test/models/user.js
+++ DevTinder/test/models/user.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
         type: Number
--- DevTinder/test/test.js
+++ DevTinder/test/test.js
@@ -1,26 +1,95 @@
-const expresss = require('express');
+const express = require('express');
 const connectDB = require('./config/database')
-const app = expresss();
-const USer = require('./models/user')
+const app = express();
+const User = require('./models/user');
+const req = require('express/lib/request');
+
+// helps handle JSON body in api calls
+app.use(express.json());
 
 app.post('/signup', async (req, res) => {
-    const userObj = {
-        firstName: "Vamsi",
-        lastName: "Kuruvella",
-        emailId: "test@gmail.com",
-        password: "xxxxx",
-        age: 23,
-        gender: "Male"
+    try {
+        const userObj = req.body
+        console.log(userObj)
+        const user = new User(userObj);
+        await user.save();
+        res.send("User added successfully");
+    } catch (err) {
+        res.status(500).send(err);
     }
-    const user = new USer(userObj);
-    await user.save();
-    res.send("User added successfully");
+});
+
+app.delete("/user", async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        // console.log(userId)
+        const del = await User.findByIdAndDelete(userId);
+        console.log(del);
+        res.send("User by Name: " + del.firstName + " " + del.lastName + " is Deleted.")
+    }
+    catch (err) {
+        res.status(500).send("Server Error");
+    }
+})
+
+app.patch('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const ret = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after" });
+        res.send("User Updated Successfully " + ret);
+    } catch (err) {
+        res.status(500).send("Server Error");
+    }
+});
+
+app.put('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const ret = await User.replaceOne({ _id: userId }, data);
+        res.send("User replaced Successfully " + ret.modifiedCount);
+    } catch (err) {
+        res.status(500).send("Server Error");
+    }
+});
+
+app.get('/feed', async (req, res) => {
+
+    try {
+        console.log(req.body);
+        if (req.body != undefined) {
+            const email = req.body.emailId;
+            const user = await User.find({ emailId: email });
+            if (user.length) {
+                res.send(user)
+            } else {
+                res.send("No User Found");
+            }
+        } else {
+            const user = await User.find({});
+            console.log(user);
+            if (user.length) {
+                res.send(user)
+            } else {
+                res.send("No User Found");
+            }
+        }
+
+    } catch (err) {
+        res.status(500).send("Server Error")
+    }
+
+});
+
+app.use("/", (req, res) => {
+    res.send("No Response");
 })
 
 connectDB().then(() => {
     console.log("Connected Successfully to DevTinder Database");
-    app.listen(7777, () => {
-        console.log("Listening on port 7777");
+    app.listen(3000, "0.0.0.0", () => {
+        console.log("Listening on port 3000");
     })
 }).catch((err) => {
     console.log("Failed with: " + err);
