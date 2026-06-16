802a1a8a7293a3131ae7832d77b700d36659ed82
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2026-06-16 17:52:15 +0530

    16/06/26 v1

--- DevTinder/DevTinder-Web/src/App.jsx
+++ DevTinder/DevTinder-Web/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World</h1>
+    <h1>Hello World</h1>
     </>
   )
 }
