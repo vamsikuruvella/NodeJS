97853d2c032edc29a115e9cd47fd678ae5ca843f
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2026-06-21 22:32:37 +0530

    21/06/26 v1

--- DevTinder/DevTinder-Web/src/login.jsx
+++ DevTinder/DevTinder-Web/src/login.jsx
@@ -1,7 +1,41 @@
 const Login = () => {
-    return<>
-        <div>Login</div>
-    </>
+    return <div className="flex justify-center items-center h-screen ">
+        <div className="card bg-base-100 w-96 shadow-sm ">
+            <div className="card-body p-[10px]">
+                
+
+                <fieldset className="fieldset py-[20px] pr-[130px] pl-[100px] rounded-box bg-base-300">
+                   <h2 className="card-title justify-center">Login</h2>
+                   <div>
+                    <legend className="fieldset-legend mb-[5px] text-[15px]">Email ID</legend>
+                    <input
+                        type="text"
+                        className="input w-[350px] mb-[20px]"
+                        placeholder=" "
+                    />
+                    </div>
+                      
+                      <div>
+                    <legend className="fieldset-legend mb-[5px] text-[15px]">Password</legend>
+                    <input
+                        type="text"
+                        className="input w-[350px]"
+                        placeholder=""  
+                    />
+                    </div>
+                     <br></br>
+                     <div className="card-actions ml-[165px]">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+                    
+                    
+                </fieldset>
+  
+
+               
+            </div>
+        </div>
+    </div>
 }
 
 export default Login
